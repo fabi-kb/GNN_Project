edd513ddc7b54e1b46cc58dd4bac2f9e21cedc03
Fabian K <fabi.89776@gmail.com>
2025-03-06 19:11:32 +0100

    Fixed Focal Loss, Fixed Pre analysis plotting, added some plots, trained multiple models on different hyperparameters

--- trainer.py
+++ trainer.py
@@ -23,20 +23,27 @@ class Trainer:
     def kl_loss(self, mu, logvar):
         return 0.5 * torch.mean(mu.pow(2) + logvar.exp() - logvar - 1)
 
-    def focal_loss(self, x, x_hat, gamma=2, alpha=0.125):
-        eps = 1e-7
-        x_hat = torch.clamp(x_hat, eps, 1 - eps)
-        return -torch.mean(
-            alpha * x * (1 - x_hat) ** gamma * torch.log(x_hat)
-            + (1 - alpha) * (1 - x) * x_hat**gamma * torch.log(1 - x_hat)
-        )
+    def focal_loss(self, x, x_hat, gamma, alpha):
+        tmp = alpha * x * (1 - x_hat).pow(gamma) * torch.log(x_hat + 1e-7) + (
+            1 - alpha
+        ) * (1 - x) * x_hat.pow(gamma) * torch.log(1 - x_hat + 1e-7)
+
+        tmp_sum = torch.sum(tmp, dim=1)
+        # print(tmp_sum.shape)
+        return -torch.mean(tmp_sum)
 
     # pre_training uses the focal loss and KL divergence loss
-    def train(self, train_loader, epochs, disk_path=None, gamma=2, alpha=0.125, beta=1):
+    def train(
+        self, train_loader, epochs, disk_path=None, gamma=2, alpha=0.125, beta=5.0
+    ):
         self.model.train()
         losses = []
+        init_beta = beta
         for epoch in range(epochs):
-
+            if epoch < self.start_decay:
+                beta = init_beta + (2.0 - init_beta) * (epoch / self.start_decay)
+            else:
+                beta = 2.0
             for i, x in enumerate(train_loader):
                 x = x.to(self.device)
                 x_hat, mu, logvar = self.model(x)
@@ -65,4 +72,6 @@ class Trainer:
                 for param_group in self.optimizer.param_groups:
                     param_group["lr"] = self.new_lr
 
-            print(f"Epoch {epoch}, Loss: {loss.item()}, LR: {self.new_lr}")
+            print(
+                f"Epoch {epoch}, Loss: {loss.item()}, LR: {self.new_lr}, Beta: {beta}, KL Loss: {beta * kl_loss}, Focal Loss: {focal_loss}"
+            )
